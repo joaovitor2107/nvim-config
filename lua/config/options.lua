b6d75a5c0110be14a7b530513e2980b000092cd7
Joao Vitor Trindade <jvtrindade21@joaos-mac-mini.local>
2026-08-20 23:00:32 -0300

    feat: add wrap line config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,3 +6,6 @@
 vim.opt.guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20"
 
 vim.opt.swapfile = false
+
+-- wrap long lines in every filetype (LazyVim default only wraps markdown/gitcommit)
+vim.opt.wrap = true
